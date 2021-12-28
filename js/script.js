a2a594ced4f47e2d5a65d5c600f7d85e33ca0210
Jay <selwynjayf@gmail.com>
2021-12-28 17:26:56 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*CART DROPDOWN*/
+const toggle = document.querySelector('#cart-toggle');
+const cart = document.querySelector('#cart');
+toggle.addEventListener('click',function(){
+if(cart.classList.contains('open')){
+    cart.classList.remove('open');
+}
+else{ 
+    cart.classList.add('open');
+}});
