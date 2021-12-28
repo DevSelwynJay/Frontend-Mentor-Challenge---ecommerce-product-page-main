c226d5587cc1b18df5a59ca7de740c2c4bd12b1a
Jay <selwynjayf@gmail.com>
2021-12-29 00:31:27 +0800

    side bar added

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ else{
     cart.classList.add('open');
 }});
 
+
+/*MOBILE MENU ASIDE*/
+const toggle_mobile_btn = document.querySelector('#mobile-menu-toggle');
+const aside = document.querySelector('#sidebar');
+const lightdrop = document.querySelector('#backdrop-light');
+const close_btn = document.querySelector('#close-mobile-menu');
+
+toggle_mobile_btn.addEventListener('click',function(){
+    aside.classList.add('show');
+    lightdrop.classList.add('show');
+});
+close_btn.addEventListener('click',function(){
+    aside.classList.remove('show');
+    lightdrop.classList.remove('show');
+});
